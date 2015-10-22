93c291d19e7453c7114d5ca1a5219e2bfab68dd3
Ryan Done <rbdone@gmail.com>
2015-10-22 13:16:21 -0600

    started migration to use rethinkdbdash

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-var r = require('rethinkdb');
 var R = require('ramda');
 var util = require('util');
 
 var Promise = require('bluebird');
-var cert = require('fs').readFileSync('./certFile');
+var cert = require('fs').readFileSync('./compose');
 
-var local = R.always({
-	host: 'localhost',
-	port: 28015
+var rLocal = require('rethinkdbdash')({
+    max: 100,
+    port: 28015,
+    host: 'rethinkdb',
+    cursor: true
 });
 
-var remote = R.always({
-	host: '',
-	authKey: '',
-	port: 1234
+var rRemote = require('rethinkdbdash')({
+    max: 100,
+    port: 1,
+    host: '',
+    authKey: '',
+    cursor: true,
+    ssl: {ca: cert}
 });
 
-var rConn = R.always(r.connect(R.assoc('ssl', {ca: cert}, remote())));
-
-var count = 1;
+var db = 'salesforce';
+var table = 'accounts';
+var insertedCount = 1;
 
 function _getUndefinedFields(document) {
-	return R.filter(fieldName => R.isNil(document[fieldName]), R.keys(document));
+    return R.filter(fieldName => R.isNil(document[fieldName]), R.keys(document));
 }
 
 function _removeUndefinedFields(document) {
-
-	return R.isArrayLike(document)
-		? R.map(document => R.omit(_getUndefinedFields(document), document), document)
-		: R.omit(_getUndefinedFields(document), document);
+    return R.isArrayLike(document)
+        ? R.map(document => R.omit(_getUndefinedFields(document), document), document)
+        : R.omit(_getUndefinedFields(document), document);
 }
-
-const db = 'salesforce';
-const table = 'accounts';
 
 var _replace = document => rConn()
-		.then(conn => r.db(db).table(table).insert(_removeUndefinedFields(document), {conflict: 'replace', returnChanges: true}).run(conn)
-			.tap(() => console.log('inserted ' + count++)));
+    .then(conn => r.db(db).table(table).insert(_removeUndefinedFields(document), {
+        conflict: 'replace',
+        returnChanges: true
+    }).run(conn)
+        .tap(() => console.log('inserted ' + insertedCount++)));
 
-var _run = () => r.connect(local())
-	.then(conn => r.db(db).table(table).run(conn)
-		.then(cursor => cursor.each(function(err, row) {
-			if (err) console.error('ERROR');
-			_replace(row);
-		}, function () {
-			cursor.close();
-			conn.close();
-		})));
+var _run = () => rRemote.db(db).table(table).run()
+    .then(cursor => cursor.each(function (err, row) {
+        if (err) console.error('ERROR');
+        console.log(row);
+        //_replace(row);
+
+    }, function () {
+        cursor.close();
+    }));
 
 module.exports = {
-	run: _run
-}
+    run: _run
+};
